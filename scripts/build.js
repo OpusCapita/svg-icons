688395ce5cc4a0c9173b5e3a0b57c76baa6944d4
Kirill Volkovich <kirill.volkovich@opuscapita.com>
2018-06-18 12:31:52 +0300

    Add "app_provisioning.svg" with hardcoded fill and size

--- scripts/build.js
+++ scripts/build.js
@@ -10,10 +10,16 @@ let destinationPath = path.resolve(__dirname, '../lib');
 fs.mkdirSync(destinationPath);
 let svgFilesNames = fs.readdirSync(sourcePath);
 
+let unprocessableFiles = [
+  'app_provisioning.svg'
+];
+
 svgFilesNames.map(svgFileName => {
   let svgSourceFilePath = path.join(sourcePath, svgFileName);
   let svgDestinationFilePath = path.join(destinationPath, svgFileName);
   let fileContent = fs.readFileSync(svgSourceFilePath, 'utf-8');
-  let readyFileContent = Object.keys(pipes).reduce((result, pipeName) => pipes[pipeName](result), fileContent);
+  let readyFileContent = unprocessableFiles.indexOf(svgFileName) === -1 ?
+    fileContent :
+    Object.keys(pipes).reduce((result, pipeName) => pipes[pipeName](result), fileContent);
   fs.writeFileSync(svgDestinationFilePath, readyFileContent);
 });
